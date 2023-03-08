7be96aa1a96b3cb50d96ce734591674c8db683a2
Akinseye Abayomi <abayomi.akinseye@gmail.com>
2023-03-08 09:53:52 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yomi on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
